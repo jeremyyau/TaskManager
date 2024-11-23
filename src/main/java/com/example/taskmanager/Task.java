f66dfed0c61398b5338b803c439fceaff6ef43c7
KwokYingFai <jeremyyaucf@gmail.com>
2024-11-24 00:44:07 +0800

    task created

--- src/main/java/com/example/taskmanager/Task.java
+++ src/main/java/com/example/taskmanager/Task.java
@@ -1,7 +1,19 @@
 package com.example.taskmanager;
 
 public class Task {
+    private int id;
     private String name;
+    private String description;
+    private int priority;
+    private int status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -10,4 +22,29 @@ public class Task {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
 }
---
+++ src/main/java/com/example/taskmanager/TaskList.java
@@ -0,0 +1,15 @@
+package com.example.taskmanager;
+
+import java.util.List;
+
+public class TaskList {
+    public List<Task> taskList;
+
+    public List<Task> getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(List<Task> taskList) {
+        this.taskList = taskList;
+    }
+}
